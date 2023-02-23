c8fe53a5ad61d8a09565ce235007eadd6f0a4a63
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2023-02-23 18:38:10 +0800

    新增跟隨系統

---
+++ UnityPetFollow/Assets/PetFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PetFollow : MonoBehaviour
+{
+    [Header("目標")]
+    [SerializeField] Transform target;
+    [Header("追蹤速度")]
+    [SerializeField] float speed = 1.5f;
+
+    private void LateUpdate()
+    {
+        Follow();
+    }
+
+    /// <summary>
+    /// 跟隨系統
+    /// </summary>
+    private void Follow()
+    {
+        Vector3 posA = transform.position;      // 取得寵物的座標資訊
+        Vector3 posB = target.position;         // 取得目標的座標資訊
+        posA.z = posB.z - 5;
+
+        Quaternion rotA = transform.rotation;
+        Quaternion rotB = target.rotation;
+        rotA.x = rotB.x;
+
+        posA = Vector3.Lerp(posA, posB, 0.2f);  // 寵物座標 = 寵物與目標的插植座標
+        rotA = Quaternion.Lerp(rotA, rotB, 0.2f);
+
+        transform.position = posA;              // 寵物座標 = 插植後的新座標
+        transform.rotation = rotA;
+    }
+}
